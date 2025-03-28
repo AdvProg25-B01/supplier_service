a56a1d99bba8093c256601b4061213903b59f372
Edmond Christian <edmond.chr@gmail.com>
2025-03-28 19:21:01 +0700

    Initial commit

---
+++ src/test/java/id/ac/ui/cs/advprog/supplier_service/SupplierServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.supplier_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplierServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
