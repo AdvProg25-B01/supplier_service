a09fdf14bc801f0da2df6470adcaf57a83ad3216
Edmond Christian <edmond.chr@gmail.com>
2025-04-11 15:12:20 +0700

    build: added SonarCloud workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
     jacoco
+    id("org.sonarqube") version "6.0.1.5171"
     id("org.springframework.boot") version "3.4.4"
     id("io.spring.dependency-management") version "1.1.7"
 }
@@ -49,4 +50,12 @@ tasks.test {
 
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "AdvProg25-B01_supplier_service")
+        property("sonar.organization", "advprog25-b01")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
