d6e8b0ebe32ea36778351dacd93e5fdae3d6cdd1
Edmond Christian <edmond.chr@gmail.com>
2025-04-11 20:39:20 +0700

    [RED] Added skeleton of SupplierController

---
+++ src/main/java/id/ac/ui/cs/advprog/supplier_service/controller/SupplierController.java
@@ -0,0 +1,26 @@
+package id.ac.ui.cs.advprog.supplier_service.controller;
+
+import id.ac.ui.cs.advprog.supplier_service.model.Supplier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface SupplierController {
+
+    @GetMapping("/suppliers")
+    ResponseEntity<List<Supplier>> getAllSuppliers();
+
+    @GetMapping("/suppliers/{id}")
+    ResponseEntity<Supplier> getSupplierById(@PathVariable UUID id);
+
+    @PostMapping("/suppliers")
+    ResponseEntity<Supplier> createSupplier(@RequestBody Supplier supplier);
+
+    @PutMapping("/suppliers/{id}")
+    ResponseEntity<Supplier> updateSupplier(@PathVariable UUID id, @RequestBody Supplier supplier);
+
+    @DeleteMapping("/suppliers/{id}")
+    ResponseEntity<Void> deleteSupplier(@PathVariable UUID id);
+}
