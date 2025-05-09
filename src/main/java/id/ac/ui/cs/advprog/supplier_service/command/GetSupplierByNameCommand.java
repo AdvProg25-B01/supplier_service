30cdb05b88d848e6435239f9415b1968437cc30d
Edmond Christian <edmond.chr@gmail.com>
2025-05-09 19:22:05 +0700

    [RED] Added skeleton for GetSupplierByNameCommand

---
+++ src/main/java/id/ac/ui/cs/advprog/supplier_service/command/GetSupplierByNameCommand.java
@@ -0,0 +1,16 @@
+package id.ac.ui.cs.advprog.supplier_service.command;
+
+import id.ac.ui.cs.advprog.supplier_service.model.Supplier;
+import id.ac.ui.cs.advprog.supplier_service.repository.SupplierRepository;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Getter
+public class GetSupplierByNameCommand implements SupplierCommand {
+    @Override
+    public List<Supplier> execute() {return null}
+}
