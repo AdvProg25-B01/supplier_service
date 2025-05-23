3613ee3cceea4d191a4ef2c68a8cf11459cfb244
Edmond Christian <edmond.chr@gmail.com>
2025-05-23 21:29:22 +0700

    [REFACTOR] Refactor SupplierServiceApplicationTests

--- src/test/java/id/ac/ui/cs/advprog/supplier_service/SupplierServiceApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/supplier_service/SupplierServiceApplicationTests.java
@@ -1,34 +1,14 @@
 package id.ac.ui.cs.advprog.supplier_service;
 
-import id.ac.ui.cs.advprog.supplier_service.command.SupplierCommandExecutor;
-import id.ac.ui.cs.advprog.supplier_service.repository.SupplierRepository;
-import id.ac.ui.cs.advprog.supplier_service.service.AsyncSupplierService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
-
-import java.util.concurrent.Executor;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class SupplierServiceApplicationTests {
 
-    @MockBean
-    private SupplierRepository supplierRepository;
-
-    @MockBean
-    private SupplierCommandExecutor commandExecutor;
-
-    @MockBean
-    private AsyncSupplierService asyncSupplierService;
-
-    @MockBean(name = "taskExecutor")
-    private Executor taskExecutor;
-
     @Test
     void contextLoads() {
-        SupplierServiceApplication.main(new String[0]);
     }
-
 }
