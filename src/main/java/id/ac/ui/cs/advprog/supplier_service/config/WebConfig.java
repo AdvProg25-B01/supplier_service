90af13bce4f51cf4a986c3fdea47265d80143184
Edmond Christian <edmond.chr@gmail.com>
2025-05-27 10:12:18 +0700

    Added cors mapping to frontend deployment

--- src/main/java/id/ac/ui/cs/advprog/supplier_service/config/WebConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/supplier_service/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://main.d2mcs3ch2l35ck.amplifyapp.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
